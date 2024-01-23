697d671e832621b7e3392394e3d30d1559a21727
MICHAEL <michaelogada104@gmail.com>
2024-01-23 18:15:34 +0000

    fix task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # 1-install_a_package.pp
 
 package { 'Flask':
-  ensure => '2.1.0',
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,9 @@
 # 2-execute_a_command.pp
 
 exec { 'killmenow':
-  command => '/usr/bin/pkill -f "killmenow"',
-  path    => '/bin:/usr/bin',
-  unless  => '/usr/bin/pgrep -f "killmenow"',
+  command    => '/usr/bin/pkill -f "killmenow"',
+  path       => '/bin:/usr/bin',
+  onlyif     => '/usr/bin/pgrep -f "killmenow"',
+  tries      => 10,        # Number of tries
+  try_sleep  => 2,         # Sleep duration between tries in seconds
 }
